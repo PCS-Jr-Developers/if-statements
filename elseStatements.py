c86eac92b57c0390e5fbad512fee2bf14c63c061
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-07 01:31:35 -0400

    Create elseStatements.py

---
+++ elseStatements.py
@@ -0,0 +1,8 @@
+# Get input from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is greater than five
+if number > 5:
+    print("The number is greater than five.")
+else:
+    print("The number is less than or equal to five.")

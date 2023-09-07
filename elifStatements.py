067db1124d1e2a9c195358049bf12fb0960f8835
Eric Doan <103901993+E-Doan@users.noreply.github.com>
2023-09-07 01:32:50 -0400

    Create elifStatements.py

---
+++ elifStatements.py
@@ -0,0 +1,10 @@
+# Get input from the user
+number = int(input("Enter a number: "))
+
+# Check conditions and print messages accordingly
+if number > 5:
+    print("The number is greater than five.")
+elif number == 5:
+    print("The number is equal to five.")
+else:
+    print("The number is less than five.")
